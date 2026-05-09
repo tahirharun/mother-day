d6fafda22ed32006c83510607db366236562b1ca
tahir <haron@gmail.com>
2026-05-09 13:57:26 -0700

    mother's day

---
+++ script.js
@@ -0,0 +1,33 @@
+const button = document.getElementById("loveBtn");
+const surprise = document.getElementById("surprise");
+
+button.addEventListener("click", () => {
+  surprise.classList.remove("hidden");
+
+  button.innerHTML = "Love You Mama ❤️";
+
+  for (let i = 0; i < 25; i++) {
+    createHeart();
+  }
+});
+
+function createHeart() {
+  const heart = document.createElement("div");
+
+  heart.classList.add("heart");
+  heart.innerHTML = "❤️";
+
+  heart.style.left = Math.random() * window.innerWidth + "px";
+
+  const size = Math.random() * 30 + 20;
+  heart.style.fontSize = size + "px";
+
+  const duration = Math.random() * 3 + 2;
+  heart.style.animationDuration = duration + "s";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, duration * 1000);
+}
